09c657025f2b906365d6080ebb5f9d97f8afd3d3
Gent Berani <gentberani@gmail.com>
2017-11-16 20:07:02 +0100

    initialize objects

--- app/src/main/java/com/gentb/cacttusedu/StopwatchActivity.java
+++ app/src/main/java/com/gentb/cacttusedu/StopwatchActivity.java
@@ -3,16 +3,32 @@ package com.gentb.cacttusedu;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.widget.Button;
+import android.widget.TextView;
 
 /**
  * Created by gentberani on 11/16/17.
  */
 
 public class StopwatchActivity extends Activity {
+
+    // UI Objects
+    TextView timeText;
+    Button startButton, stopButton, resetButton;
+
+    // UI Objects
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stopwatch);
+        initializeViews();
 
+    }
+
+    private void initializeViews() {
+        timeText = findViewById(R.id.timeText);
+        startButton = findViewById(R.id.startButton);
+        stopButton = findViewById(R.id.stopButton);
+        resetButton = findViewById(R.id.resetButton);
     }
 }
