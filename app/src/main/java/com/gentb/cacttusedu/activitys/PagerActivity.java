78e6601a94b30fe4e25129d4a7c77c8226a85fbd
Gent Berani <gentberani@gmail.com>
2017-11-27 20:14:51 +0100

    add: viewpager

---
+++ app/src/main/java/com/gentb/cacttusedu/activitys/PagerActivity.java
@@ -0,0 +1,34 @@
+package com.gentb.cacttusedu.activitys;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.view.ViewPager;
+
+import com.gentb.cacttusedu.R;
+import com.gentb.cacttusedu.adapters.ViewPagerAdapter;
+
+/**
+ * Created by Ndriqim Haxhaj on 11/27/2017.
+ */
+
+public class PagerActivity extends Activity {
+
+    ViewPager viewPager;
+    ViewPagerAdapter adapter;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pager);
+
+        viewPager = findViewById(R.id.viewPager);
+        adapter = new ViewPagerAdapter(getApplicationContext());
+        viewPager.setAdapter(adapter);
+        adapter.notifyDataSetChanged();
+
+    }
+}
+
+
+
---
+++ app/src/main/java/com/gentb/cacttusedu/adapters/ViewPagerAdapter.java
@@ -0,0 +1,67 @@
+package com.gentb.cacttusedu.adapters;
+
+import android.content.Context;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.gentb.cacttusedu.R;
+
+import org.w3c.dom.Text;
+
+/**
+ * Created by Ndriqim Haxhaj on 11/27/2017.
+ */
+
+public class ViewPagerAdapter extends PagerAdapter {
+
+
+    Context context;
+    public ViewPagerAdapter(Context context){
+        this.context = context;
+    }
+
+    @Override
+    public Object instantiateItem(ViewGroup container, int position) {
+
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+        View customView = inflater.inflate(R.layout.pager_cell,container,false);
+        System.out.println("fff");
+        TextView title = (TextView) customView.findViewById(R.id.titleTextView);
+        title.setText(""+position);
+        container.addView(customView);
+        return customView;
+    }
+
+
+
+    @Override
+    public int getCount() {
+        return 10;
+    }
+
+    @Override
+    public boolean isViewFromObject(View view, Object object) {
+        return view.equals(object);
+    }
+
+    @Override
+    public int getItemPosition(Object object) {
+        return POSITION_NONE;
+    }
+
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object) {
+        super.destroyItem(container, position, object);
+    }
+
+    @Override
+    public void destroyItem(View container, int position, Object object) {
+        ((ViewPager) container).removeView((View) object);
+
+    }
+}
