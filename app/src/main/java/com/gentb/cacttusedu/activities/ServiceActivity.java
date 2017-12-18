2cc03701b95e9e5400d6a84c87cfd6d4a3261dde
Gent Berani <gentberani@gmail.com>
2017-12-19 00:20:14 +0100

    add asynctask

--- app/src/main/java/com/gentb/cacttusedu/activities/ServiceActivity.java
+++ app/src/main/java/com/gentb/cacttusedu/activities/ServiceActivity.java
@@ -1,11 +1,19 @@
 package com.gentb.cacttusedu.activities;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.support.annotation.Nullable;
 
 import com.gentb.cacttusedu.R;
+
+import java.io.IOException;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 /**
  * Created by gentberani on 12/18/17.
@@ -13,10 +21,57 @@ import com.gentb.cacttusedu.R;
 
 public class ServiceActivity extends Activity {
 
-    @Override
-    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
-        super.onCreate(savedInstanceState, persistentState);
-        setContentView(R.layout.activity_service);
-    }
 
+    @SuppressLint("StaticFieldLeak")
+    @Overridegit
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_service);
+
+
+        new AsyncTask<String, Integer, String>() {
+
+            @Override
+            protected void onPreExecute() {
+                super.onPreExecute();
+            }
+
+            @Override
+            protected String doInBackground(String... strings) {
+
+
+                final String url = "http://10.45.0.34:8888/cacttusedu/index.php?Lendet&UserID=1";
+                final OkHttpClient client = new OkHttpClient();
+
+                Request request = new Request.Builder()
+                        .url(url)
+                        .build();
+
+                Response response = null;
+
+                try {
+                    response = client.newCall(request).execute();
+                    final String stringResponse = response.body().string();
+                    return stringResponse;
+                } catch (IOException e) {
+                    e.printStackTrace();
+
+                }
+
+                return null;
+            }
+
+            @Override
+            protected void onProgressUpdate(Integer... values) {
+                super.onProgressUpdate(values);
+            }
+
+            @Override
+            protected void onPostExecute(String response) {
+                super.onPostExecute(response);
+                System.out.println("response: " + response);
+
+            }
+        }.execute();
+    }
 }
---
+++ app/src/main/java/com/gentb/cacttusedu/network/LendetCallback.java
@@ -0,0 +1,9 @@
+package com.gentb.cacttusedu.network;
+
+/**
+ * Created by gentberani on 12/18/17.
+ */
+
+public interface LendetCallback {
+    void onLendetResponse();
+}
